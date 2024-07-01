216f229a8942106632a679e73a49abe1f2d73cf9
liankan1359 <140187454+liankan1359@users.noreply.github.com>
2024-07-01 17:35:54 +0700

    navbar

---
+++ Script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuButtons = document.querySelectorAll("#menu-btn");
+    const navDialog = document.getElementById("nav-dialog");
+
+    menuButtons.forEach(btn => {
+        btn.addEventListener("click", () => {
+            navDialog.classList.toggle("hidden");
+        });
+    });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors:{
+        'primary': '#3238f2',
+      },
+      fontFamily:{
+        'display':['Inter', 'sans-serif'],
+        'body':['Inter', 'sans-serif']
+      }
+
+    },
+  },
+  plugins: [],
+}
+
